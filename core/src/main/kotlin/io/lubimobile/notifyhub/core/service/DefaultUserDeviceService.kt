f76dddaf2aa692240969439bc97c36bef8c7f6a0
NikitaLyubimtsev <LyubimtsevN.A@yandex.ru>
2025-12-10 14:15:11 +0300

    fix: work on update user device

--- core/src/main/kotlin/io/lubimobile/notifyhub/core/service/DefaultUserDeviceService.kt
+++ core/src/main/kotlin/io/lubimobile/notifyhub/core/service/DefaultUserDeviceService.kt
@@ -3,19 +3,29 @@ package io.lubimobile.notifyhub.core.service
 import io.lubimobile.notifyhub.core.api.UserDeviceRepository
 import io.lubimobile.notifyhub.core.api.UserDeviceService
 import io.lubimobile.notifyhub.core.model.UserDevice
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import kotlin.time.Instant
 
 @Service
 class DefaultUserDeviceService(
     private val repository: UserDeviceRepository
 ) : UserDeviceService {
 
+    private val log = LoggerFactory.getLogger(DefaultUserDeviceService::class.java)
+
     @Transactional
     override fun saveUserDevice(userDevice: UserDevice) {
         val existingDevice = repository.findDeviceByDeviceToken(userDevice.deviceToken)
+        log.debug("Saving user device {}", existingDevice)
         if (existingDevice != null) {
-            repository.save(existingDevice.copy(userId = userDevice.userId, deviceId = userDevice.deviceId))
+            val updateDevice = existingDevice.copy(
+                userId = userDevice.userId,
+                deviceId = userDevice.deviceId,
+            )
+            log.debug("Saving user update device data {}", updateDevice)
+            repository.save(updateDevice)
         } else {
             repository.save(userDevice)
         }
